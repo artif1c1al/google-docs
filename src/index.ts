e922629e06ca8442da567e945fcdd43e6f2b34cf
artif1c1al <zhuravlenkoartem@gmail.com>
2020-09-05 15:57:10 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as _ from 'lodash';
+
+import './style.scss';
+
+
+function component() {
+  // const element = document.createElement('div');
+  // console.log('hi');
+
+  // Lodash, currently included via a script, is required for this line to work
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
+
+  // return element;
+}
+
+// document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+
+  devServer: {
+    stats: {
+      children: false, // Hide children information
+      maxModules: 0 // Set the maximum number of modules to be shown
+    },
+    // contentBase: './dist',
+    port: 3001
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  output: {
+    publicPath: 'dist', // Как это работает?
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+
+  // plugins: [new HtmlWebpackPlugin()],
+
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+
+
+};
